0b8f91a3cf6bc80ee20746f648018b6e1d3f1922
Alexandra Manoliu <alexandra.manoliu@gmail.com>
2016-10-10 18:05:36 +0200

    first commit

---
+++ Lower_case.py
@@ -0,0 +1,2 @@
+string = "Where would this come handy? For example if you ask user \"Would you like to continue (yes/no)?\", the user might respond: \"yes\", \"Yes\", \"YES\" or even \"YeS\". In this case, changing your user\'s response into lowercase letters would be very helpful in your if-else statement."
+print string.lower()
